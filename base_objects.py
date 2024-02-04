cd25ac96222487332bc2a9cffdc353a850cf39cd
Brad Wyatt <bradjr@gmail.com>
2024-02-03 21:54:37 -0800

    separating placed objects

--- base_objects.py
+++ base_objects.py
@@ -5,3 +5,28 @@ class StartGameObject(pygame.sprite.Sprite):
         super().__init__()
         self.image = image  # Assume image is already loaded
         self.rect = self.image.get_rect(topleft=position)
+        
+class PlacedObject(pygame.sprite.Sprite):
+    object_list = []  # This could be overridden by subclasses if separate lists are needed
+
+    def __init__(self, pos, placed_sprites, image):
+        super().__init__()
+        self.image = image
+        self.rect = self.image.get_rect(topleft=pos)
+        placed_sprites.add(self)
+        self.add_to_class_list()
+    
+    def add_to_class_list(self):
+        # This method will be overridden by subclasses to add the object to the specific list
+        pass
+
+    def update(self):
+        pass
+
+    def destroy(self):
+        self.remove_from_class_list()
+        self.kill()
+    
+    def remove_from_class_list(self):
+        # This method will be overridden by subclasses to remove the object from the specific list
+        pass
---
+++ placed_objects.py
@@ -0,0 +1,133 @@
+from base_objects import PlacedObject
+
+class PlacedWall(PlacedObject):
+    wall_list = []
+
+    def __init__(self, pos, placed_sprites, images):
+        super().__init__(pos, placed_sprites, images["spr_wall"])
+
+    def add_to_class_list(self):
+        PlacedWall.wall_list.append(self)
+
+    def remove_from_class_list(self):
+        PlacedWall.wall_list.remove(self)
+        
+class PlacedReverseWall(PlacedObject):
+    reverse_wall_list = []
+
+    def __init__(self, pos, placed_sprites, images):
+        super().__init__(pos, placed_sprites, images["spr_reverse_wall"])
+
+    def add_to_class_list(self):
+        PlacedReverseWall.reverse_wall_list.append(self)
+
+    def remove_from_class_list(self):
+        PlacedReverseWall.reverse_wall_list.remove(self)
+        
+class PlacedDiamonds(PlacedObject):
+    diamonds_list = []
+
+    def __init__(self, pos, placed_sprites, images):
+        super().__init__(pos, placed_sprites, images["spr_diamonds"])
+
+    def add_to_class_list(self):
+        PlacedDiamonds.diamonds_list.append(self)
+
+    def remove_from_class_list(self):
+        PlacedDiamonds.diamonds_list.remove(self)
+        
+class PlacedDoor(PlacedObject):
+    door_list = []
+
+    def __init__(self, pos, placed_sprites, images):
+        super().__init__(pos, placed_sprites, images["spr_door_closed"])
+
+    def add_to_class_list(self):
+        PlacedDoor.door_list.append(self)
+
+    def remove_from_class_list(self):
+        PlacedDoor.door_list.remove(self)
+        
+class PlacedFlyer(PlacedObject):
+    flyer_list = []
+
+    def __init__(self, pos, placed_sprites, images):
+        super().__init__(pos, placed_sprites, images["spr_flyer"])
+
+    def add_to_class_list(self):
+        PlacedFlyer.flyer_list.append(self)
+
+    def remove_from_class_list(self):
+        PlacedFlyer.flyer_list.remove(self)
+        
+class PlacedSmilyRobot(PlacedObject):
+    smily_robot_list = []
+
+    def __init__(self, pos, placed_sprites, images):
+        super().__init__(pos, placed_sprites, images["spr_smily_robot"])
+
+    def add_to_class_list(self):
+        PlacedSmilyRobot.smily_robot_list.append(self)
+
+    def remove_from_class_list(self):
+        PlacedSmilyRobot.smily_robot_list.remove(self)
+        
+class PlacedSpring(PlacedObject):
+    spring_list = []
+
+    def __init__(self, pos, placed_sprites, images):
+        super().__init__(pos, placed_sprites, images["spr_spring"])
+
+    def add_to_class_list(self):
+        PlacedSpring.spring_list.append(self)
+
+    def remove_from_class_list(self):
+        PlacedSpring.spring_list.remove(self)
+        
+class PlacedStickyBlock(PlacedObject):
+    sticky_block_list = []
+
+    def __init__(self, pos, placed_sprites, images):
+        super().__init__(pos, placed_sprites, images["spr_sticky_block"])
+
+    def add_to_class_list(self):
+        PlacedStickyBlock.sticky_block_list.append(self)
+
+    def remove_from_class_list(self):
+        PlacedStickyBlock.sticky_block_list.remove(self)
+        
+class PlacedFallSpikes(PlacedObject):
+    fall_spikes_list = []
+
+    def __init__(self, pos, placed_sprites, images):
+        super().__init__(pos, placed_sprites, images["spr_fall_spikes"])
+
+    def add_to_class_list(self):
+        PlacedFallSpikes.fall_spikes_list.append(self)
+
+    def remove_from_class_list(self):
+        PlacedFallSpikes.fall_spikes_list.remove(self)
+        
+class PlacedStandSpikes(PlacedObject):
+    stand_spikes_list = []
+
+    def __init__(self, pos, placed_sprites, images):
+        super().__init__(pos, placed_sprites, images["spr_stand_spikes"])
+
+    def add_to_class_list(self):
+        PlacedStandSpikes.stand_spikes_list.append(self)
+
+    def remove_from_class_list(self):
+        PlacedStandSpikes.stand_spikes_list.remove(self)
+        
+class PlacedPlayer(PlacedObject):
+    player_list = []
+
+    def __init__(self, pos, placed_sprites, images):
+        super().__init__(pos, placed_sprites, images["spr_player"])
+
+    def add_to_class_list(self):
+        PlacedPlayer.player_list.append(self)
+
+    def remove_from_class_list(self):
+        PlacedPlayer.player_list.remove(self)
